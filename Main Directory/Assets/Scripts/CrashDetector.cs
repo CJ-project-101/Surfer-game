d751998fbbc471215409aedb6346bb7101ab4694
Calvin K J <calvinkj412@gmail.com>
2026-02-25 20:00:33 +0530

    Improved asthetics, finish line and death particles, finishline and death logics

--- Main Directory/Assets/Scripts/CrashDetector.cs
+++ Main Directory/Assets/Scripts/CrashDetector.cs
@@ -1,13 +1,23 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CrashDetector : MonoBehaviour
 {
+    [SerializeField] float Delay=0.5f;
+    [SerializeField] ParticleSystem crashParticle; 
     void  OnTriggerEnter2D(Collider2D other)
     {
-        int layerIndex= LayerMask.NameToLayer("Player");
+        int layerIndex= LayerMask.NameToLayer("Floor");
         if(other.gameObject.layer==layerIndex  )
         {
-            Debug.Log("You lose!");
+            crashParticle.Play();
+            Invoke("ReloadScene", Delay);
         }
     }   
+
+    void ReloadScene()
+    {
+        SceneManager.LoadScene(0);
+
+    }
 }
