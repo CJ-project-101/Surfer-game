a022eb4a86922b2992b68db9d67ddef2c280afa3
Calvin K J <calvinkj412@gmail.com>
2026-02-26 19:49:44 +0530

    Some Adujustments made towards the game

--- Main Directory/Assets/Scripts/PlayerCOntroller.cs
+++ Main Directory/Assets/Scripts/PlayerCOntroller.cs
@@ -3,21 +3,37 @@ using UnityEngine.InputSystem;
 
 public class PlayerCOntroller : MonoBehaviour
 {
-    [SerializeField] float torqueAmount = 1f;
+    [SerializeField] float torqueAmount = 10f;
+    [SerializeField] float baseSpeed = 15f;
+    [SerializeField] float boostSpeed = 20f;
+    
     Rigidbody2D myRigidbody2D;
     InputAction moveAction;
+    SurfaceEffector2D surfaceEffector2D;
+    Vector2 moveVector;
+     bool canControlPlayer = true; 
+
     void Start()
     {
 
         moveAction = InputSystem.actions.FindAction("Move");
         myRigidbody2D = GetComponent<Rigidbody2D>();
+        surfaceEffector2D = FindFirstObjectByType<SurfaceEffector2D>();
 
     }
 
-
     void Update()
     {
-        Vector2 moveVector;
+        if(canControlPlayer)
+        {
+            RoatatePlayer();
+            BoostPlayer();
+        }
+    }
+
+    void RoatatePlayer()
+    {
+        
         moveVector= moveAction.ReadValue<Vector2>();
         if(moveVector.x > 0)
         {
@@ -27,6 +43,20 @@ public class PlayerCOntroller : MonoBehaviour
         {
             myRigidbody2D.AddTorque(torqueAmount);
         }
-       
+    }
+
+    void BoostPlayer()
+    {
+        if (moveVector.y > 0)
+        {
+            surfaceEffector2D.speed = boostSpeed;
+        }
+        else if(moveVector.y <= 0){
+            surfaceEffector2D.speed = baseSpeed;
+        }
+    }
+
+    public void DisableControl(){
+        canControlPlayer = false;
     }
 }
